2cf391a5bfba959708696eb917b5dd8b84653af2
kj5377701 <77152005+kj5377701@users.noreply.github.com>
2021-02-17 12:01:43 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((socket.gethostname(), 1234))
+
+server.listen(5)
+
+serversocket, addr = server.accept()
+
+print('connect from: %s' % addr[0])
+
+data = serversocket.recv(1024)
+
+serversocket.send(data)
+
+serversocket.close()
+
+server.close()
